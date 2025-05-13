96b6669521daa540f834037308fcf57c9965eb98
Eric <emoolenbeek@gmail.com>
2025-05-13 19:07:07 -0400

    display nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import '../app.css';
+	import Navigation from "$lib/components/navigation/navigation.svelte";
 	let { children } = $props();
 </script>
 
+<Navigation />
 {@render children()}
