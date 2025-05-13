1e69aa0b71617fe1d3d4c67cfe07a8c213ea0493
Eric <emoolenbeek@gmail.com>
2025-05-13 19:04:51 -0400

    create nav

---
+++ src/lib/components/navigation/navigation.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+</script>
+
+<nav class="flex justify-between items-center p-4 bg-primary text-primary-foreground">
+    <a href="/">
+        <h1>Website</h1>
+    </a>
+</nav>
