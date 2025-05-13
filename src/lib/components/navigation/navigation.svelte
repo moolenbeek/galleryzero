c236d2f2f2119e06580b7216c475e9b81ccf6e5b
Eric <emoolenbeek@gmail.com>
2025-05-13 19:05:28 -0400

    update nav

--- src/lib/components/navigation/navigation.svelte
+++ src/lib/components/navigation/navigation.svelte
@@ -1,7 +1,3 @@
-<script lang="ts">
-    import { page } from '$app/stores';
-</script>
-
 <nav class="flex justify-between items-center p-4 bg-primary text-primary-foreground">
     <a href="/">
         <h1>Website</h1>
