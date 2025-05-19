ee14db91d549b7accabdee9200fae3f8e74e96ae
Eric <emoolenbeek@gmail.com>
2025-05-19 15:26:26 -0400

    add card to login

--- src/lib/components/admin/login.svelte
+++ src/lib/components/admin/login.svelte
@@ -2,21 +2,30 @@
 	import { enhance } from '$app/forms';
 	import type { ActionData } from '../../../routes/admin/login/$types';
     import { Button } from '$lib/components/ui/button';
-
+    import * as Card from "$lib/components/ui/card/index.js";
+	import { Input } from '$lib/components/ui/input';
 	export let form: ActionData;
 </script>
 
-<h1>Login/Register</h1>
-<form method='post' action='?/login' use:enhance>
-	<label>
-		Username
-		<input name='username' />
-	</label>
-	<label>
-		Password
-		<input type='password' name='password' />
-	</label>
-	<Button type="submit">Login</Button>
-	<Button formaction='?/register'>Register</Button>
-</form>
-<p style='color: red'>{form?.message ?? ''}</p>
+<Card.Root>
+    <Card.Header>
+        <Card.Title>Login/Register</Card.Title>
+    </Card.Header>
+	<Card.Content>
+		<form method='post' action='?/login' use:enhance>
+			<label>
+				Username
+				<Input name='username' />
+			</label>
+			<label>
+				Password
+				<Input type='password' name='password' />
+			</label>
+			<div class="flex gap-2 pt-4">
+				<Button type="submit">Login</Button>
+				<Button formaction='?/register'>Register</Button>
+			</div>
+		</form>
+		<p style='color: red'>{form?.message ?? ''}</p>
+	</Card.Content>
+</Card.Root>
