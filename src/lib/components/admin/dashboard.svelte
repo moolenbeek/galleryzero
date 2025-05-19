a9dbc29edb723b34fcd1c8d352aa507f95c5d1b1
Eric <emoolenbeek@gmail.com>
2025-05-19 16:11:14 -0400

    add card for gallery cms

--- src/lib/components/admin/dashboard.svelte
+++ src/lib/components/admin/dashboard.svelte
@@ -6,9 +6,10 @@
     export let data: { user: { id: string; username: string; role: string } };
 </script>
 
-<h1>Dashboard Component</h1>
-
 <Card.Root>
+    <Card.Header>
+        <Card.Title>User</Card.Title>
+    </Card.Header>
     <Card.Content>
         <p>Hi {data.user.username}</p>
         <form method="post" action="?/logout" use:enhance>
@@ -16,5 +17,14 @@
                 <Button type="submit">Log out</Button>  
             </div>
         </form>
+    </Card.Content>
+</Card.Root>
+
+<Card.Root class="mt-4">
+    <Card.Header>
+        <Card.Title>Gallery</Card.Title>
+    </Card.Header>
+    <Card.Content>
+        <p>Gallery</p>
     </Card.Content>
 </Card.Root>
